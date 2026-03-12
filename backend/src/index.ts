961e3e56b2ff5b932f11a92edbbd5b2478828bea
sigmajulio <rvuturaf2@gmail.com>
2026-03-11 22:44:09 -0600

    fix: added vercel url to cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,8 @@ import { routes } from "./routes/index";
 await connectDB();
 
 const app = new Elysia()
-  .use(cors())
+  .use(cors({ origin:"https://technical-assesment-test.vercel.app", "http://localhost:5173"],
+}))
   .use(routes)
   .get("/", () => ({ message: "Products API v1 - Running ✅" }))
   .listen(process.env.PORT || 3000);
