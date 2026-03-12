9be5ab78446651379b9e0ffcb7b5c825eb968d50
sigmajulio <rvuturaf2@gmail.com>
2026-03-11 22:50:21 -0600

    fix: set railway api url in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/v1",
+  baseURL: "https://technicalassesmenttest-production.up.railway.app/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 
